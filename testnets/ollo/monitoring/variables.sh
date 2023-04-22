42ea5348e65a04a11386b4792ce5e60781ab571f
NktSH <49846495+NktSH@users.noreply.github.com>
2023-04-22 20:00:18 +0300

    Update variables.sh

--- testnets/ollo/monitoring/variables.sh
+++ testnets/ollo/monitoring/variables.sh
@@ -1,5 +1,5 @@
 #Ollo monitoring variables template 
-COS_BIN_NAME=insert_path_to_nibid_binary # example: /root/go/bin/ollod or /home/user/go/bin/ollod
+COS_BIN_NAME=insert_path_to_ollod_binary # example: /root/go/bin/ollod or /home/user/go/bin/ollod
 COS_PORT_RPC=insert_port_here              # default: 26657
 COS_VALOPER=insert_valoper_address         # example: ollovaloper1234545636767376535673
 LOGENTRY=ollo
