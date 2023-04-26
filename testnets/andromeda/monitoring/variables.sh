7e908bb2f378fedda025404628add918c2818b48
NktSH <49846495+NktSH@users.noreply.github.com>
2023-04-26 18:39:29 +0300

    Create variables.sh

---
+++ testnets/andromeda/monitoring/variables.sh
@@ -0,0 +1,5 @@
+#Andromeda monitoring variables template 
+COS_BIN_NAME=          # use which andromedad command to get the path. Example: /root/go/bin/andromedad or /home/user/go/bin/andromedad
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: andrvaloper1234545636767376535673
+LOGENTRY=andromeda
