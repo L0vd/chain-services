d59593d34c7dc24a8bb82a086b12bda00b81870e
NktSH <49846495+NktSH@users.noreply.github.com>
2023-04-26 19:35:08 +0300

    Create variables.sh

---
+++ testnets/ojo/monitoring/variables.sh
@@ -0,0 +1,5 @@
+#Ojo monitoring variables template 
+COS_BIN_NAME=          # use "which ojod" command to get the path. Example: /root/go/bin/ojod or /home/user/go/bin/ojod
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: andrvaloper1234545636767376535673
+LOGENTRY=ojo
