8b63587404ea64b1e695a4a4804083a85492a11e
Egozit <iampetroved@gmail.com>
2023-04-27 15:40:17 +0200

    Create gitbook content

---
+++ mainnets/nois/monitoring/variables.sh
@@ -0,0 +1,5 @@
+#Nois monitoring variables template 
+COS_BIN_NAME=          # example: /root/go/bin/noisd or /home/user/go/bin/noisd
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: noisvaloper1234545636767376535673
+LOGENTRY=nois
