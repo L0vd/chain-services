cfcb4d670d0aef4d320208a947c8a9874da38710
NktSH <49846495+NktSH@users.noreply.github.com>
2023-05-23 21:10:00 +0300

    Create variables.sh

---
+++ testnets/bonusblock/monitoring/variables.sh
@@ -0,0 +1,5 @@
+#Bonusblock monitoring variables template 
+COS_BIN_NAME=          # use "which bonus-blockd" command to get the path. Example: /root/go/bin/bonus-blockd or /home/user/go/bin/bonus-blockd
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: bonusvaloper1234545636767376535673
+LOGENTRY=bonusblock
