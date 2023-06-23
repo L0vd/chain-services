7848ddff45354c09caa6f7b5ef53cbb0da2f6cd4
NktSH <49846495+NktSH@users.noreply.github.com>
2023-06-23 14:26:42 +0300

    Create variables.sh

---
+++ mainnets/haqq/monitoring/variables.sh
@@ -0,0 +1,5 @@
+#HAQQ monitoring variables template 
+COS_BIN_NAME=          # example: /root/go/bin/haqqd or /home/user/go/bin/haqqd
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: haqqvaloper1234545636767376535673
+LOGENTRY=haqq
