9c9c3a3bd82497f48502ab56f12f489b205306d6
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-03 18:07:38 +0300

    Delete mainnets/cosmoshub/monitoring directory

--- mainnets/cosmoshub/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Cosmoshub monitoring variables template 
-COS_BIN_NAME=          # use "which gaiad" command to get the path. Example: /root/go/bin/gaiad or /home/user/go/bin/gaiad
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: nullvaloper1234545636767376535673
-LOGENTRY=cosmoshub
