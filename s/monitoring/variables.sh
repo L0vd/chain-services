dc2cb594316e12ce6cbfa079d0246c79bb6d9dc4
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-05 01:31:14 +0300

    Delete s directory

--- s/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#  monitoring variables template 
-COS_BIN_NAME=          # use "which " command to get the path. Example: /root/go/bin/ or /home/user/go/bin/
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: valoper1234545636767376535673
-LOGENTRY=
