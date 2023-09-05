f80ab13ed25cd61095bc2d5991164419cee713d3
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-05 20:11:51 +0300

    Delete testnets/archway/monitoring directory

--- testnets/archway/monitoring/variables.sh
+++
@@ -1,5 +0,0 @@
-#archway monitoring variables template 
-COS_BIN_NAME=          # use "which archwayd" command to get the path. Example: /root/go/bin/archwayd or /home/user/go/bin/archwayd
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: archwayvaloper1234545636767376535673
-LOGENTRY=archway
