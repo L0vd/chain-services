2370ebc352a36ec35dbc1d0ba1eb4baf1c549986
Egozit <iampetroved@gmail.com>
2023-09-05 19:18:42 +0200

    Update gitbook

---
+++ testnets/noria/monitoring/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Noria monitoring variables template 
+COS_BIN_NAME=          # use "which noriad" command to get the path. Example: /root/go/bin/noriad or /home/user/go/bin/noriad
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: noriavaloper1234545636767376535673
+LOGENTRY=noria
