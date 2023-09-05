60044194c767233594bb3fabdaae99e2db1fb67a
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-05 20:11:36 +0300

    Delete testnets/noria/monitoring directory

--- testnets/noria/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Noria monitoring variables template 
-COS_BIN_NAME=          # use "which noria" command to get the path. Example: /root/go/bin/noria or /home/user/go/bin/noria
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: noriavaloper1234545636767376535673
-LOGENTRY=noria
