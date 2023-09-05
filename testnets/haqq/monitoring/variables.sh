4e9d7ddf6363ddd7912aa445cabbf63173bb1aa2
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-05 20:10:50 +0300

    Delete testnets/haqq/monitoring directory

--- testnets/haqq/monitoring/variables.sh
+++
@@ -1,5 +0,0 @@
-#HAQQ monitoring variables template 
-COS_BIN_NAME=          # example: /root/go/bin/haqqd or /home/user/go/bin/haqqd
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: haqqvaloper1234545636767376535673
-LOGENTRY=haqq
