9fa2c5a1d912b0a50ee9e46ead2a4e76b56ac5a8
Egozit <iampetroved@gmail.com>
2023-09-05 15:41:09 +0200

    Update gitbook

---
+++ mainnets/haqq/monitoring/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Haqq monitoring variables template 
+COS_BIN_NAME=          # use "which haqqd" command to get the path. Example: /root/go/bin/haqqd or /home/user/go/bin/haqqd
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: haqqvaloper1234545636767376535673
+LOGENTRY=haqq
