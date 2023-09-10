0f1ec60a3389b645d2154e35e8989638801cf874
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-10 22:26:18 +0300

    Delete testnets/arkeo/monitoring directory

--- testnets/arkeo/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Arkeo monitoring variables template 
-COS_BIN_NAME=          # use "which Arkeo" command to get the path. Example: /root/go/bin/Arkeo or /home/user/go/bin/Arkeo
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: tarkeovaloper1234545636767376535673
-LOGENTRY=arkeo
