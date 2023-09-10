850fc6e8cc1f90c5c0b3836da77c363b8f0ad970
Egozit <iampetroved@gmail.com>
2023-09-10 21:18:20 +0200

    Update gitbook

---
+++ testnets/arkeo/monitoring/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Arkeo monitoring variables template 
+COS_BIN_NAME=          # use "which Arkeo" command to get the path. Example: /root/go/bin/Arkeo or /home/user/go/bin/Arkeo
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: tarkeovaloper1234545636767376535673
+LOGENTRY=arkeo
