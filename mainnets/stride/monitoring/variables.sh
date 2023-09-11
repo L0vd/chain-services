64b5a2bbaa05b0e0862e8b06ed40de2932ad0a08
Egozit <iampetroved@gmail.com>
2023-09-11 18:54:43 +0200

    Update gitbook

---
+++ mainnets/stride/monitoring/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Stride monitoring variables template 
+COS_BIN_NAME=          # use "which strided" command to get the path. Example: /root/go/bin/strided or /home/user/go/bin/strided
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: stridevaloper1234545636767376535673
+LOGENTRY=stride
