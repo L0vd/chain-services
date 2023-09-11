5dc57605ae8305e143dd970f2751d916beb5a5c6
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-11 19:38:07 +0300

    Delete mainnets/osmosis/monitoring directory

--- mainnets/osmosis/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Osmosis monitoring variables template 
-COS_BIN_NAME=          # use "which osmosisd" command to get the path. Example: /root/go/bin/osmosisd or /home/user/go/bin/osmosisd
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: nullvaloper1234545636767376535673
-LOGENTRY=osmosis
