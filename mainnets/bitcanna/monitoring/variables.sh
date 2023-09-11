1eb352131df22e1479666b2defd644e099034192
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-11 19:36:21 +0300

    Delete mainnets/bitcanna/monitoring directory

--- mainnets/bitcanna/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Bitcanna monitoring variables template 
-COS_BIN_NAME=          # use "which bcnad" command to get the path. Example: /root/go/bin/bcnad or /home/user/go/bin/bcnad
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: bcnavaloper1234545636767376535673
-LOGENTRY=bitcanna
