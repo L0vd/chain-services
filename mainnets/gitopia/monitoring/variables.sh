14ec881987e1350f602e8f3dd9545eaee940c9d3
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-11 19:37:22 +0300

    Delete mainnets/gitopia/monitoring directory

--- mainnets/gitopia/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Gitopia monitoring variables template 
-COS_BIN_NAME=          # use "which gitopiad" command to get the path. Example: /root/go/bin/gitopiad or /home/user/go/bin/gitopiad
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: gitopiavaloper1234545636767376535673
-LOGENTRY=gitopia
