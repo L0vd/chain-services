101a41a0a9d649eedeed1f892c0ec2a15bb74b09
Egozit <iampetroved@gmail.com>
2023-09-11 18:55:19 +0200

    Update gitbook

---
+++ mainnets/composable/monitoring/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Composable monitoring variables template 
+COS_BIN_NAME=          # use "which centaurid" command to get the path. Example: /root/go/bin/centaurid or /home/user/go/bin/centaurid
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: centaurivaloper1234545636767376535673
+LOGENTRY=composable
