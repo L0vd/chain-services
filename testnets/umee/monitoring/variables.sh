acdf1c34c41b5fdf69a73b1a5092e398d1c3b84e
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-11 19:39:57 +0300

    Delete testnets/umee/monitoring directory

--- testnets/umee/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Umee monitoring variables template 
-COS_BIN_NAME=          # use "which umeed" command to get the path. Example: /root/go/bin/umeed or /home/user/go/bin/umeed
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: umeevaloper1234545636767376535673
-LOGENTRY=umee
