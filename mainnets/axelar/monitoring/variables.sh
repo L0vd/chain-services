87437b4abf3aa13c7a62e08495f97b6c3a728275
NktSH <49846495+NktSH@users.noreply.github.com>
2023-09-11 19:36:09 +0300

    Delete mainnets/axelar/monitoring directory

--- mainnets/axelar/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Axelar monitoring variables template 
-COS_BIN_NAME=          # use "which axelard" command to get the path. Example: /root/go/bin/axelard or /home/user/go/bin/axelard
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: nullvaloper1234545636767376535673
-LOGENTRY=axelar
