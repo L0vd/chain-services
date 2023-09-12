bb2a3178b60c085cb4473688ce3e9af14fd35523
Egozit <iampetroved@gmail.com>
2023-09-12 17:39:18 +0200

    Update gitbook

---
+++ testnets/lava/monitoring/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Lava monitoring variables template 
+COS_BIN_NAME=          # use "which lavad" command to get the path. Example: /root/go/bin/lavad or /home/user/go/bin/lavad
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: lava@valoper1234545636767376535673
+LOGENTRY=lava
