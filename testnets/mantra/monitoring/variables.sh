cc6217a949a3323626b657038b591803721a3c14
Egozit <iampetroved@gmail.com>
2023-10-11 21:33:24 +0200

    Update gitbook

---
+++ testnets/mantra/monitoring/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Mantra monitoring variables template 
+COS_BIN_NAME=          # use "which mantrachaind" command to get the path. Example: /root/go/bin/mantrachaind or /home/user/go/bin/mantrachaind
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: mantravaloper1234545636767376535673
+LOGENTRY=mantra
