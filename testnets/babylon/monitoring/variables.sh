455bd2fe118ce99a87cf62480873a1ca7ef901a6
Egozit <iampetroved@gmail.com>
2023-10-17 17:52:11 +0200

    Update gitbook

---
+++ testnets/babylon/monitoring/variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Babylon monitoring variables template 
+COS_BIN_NAME=          # use "which babylond" command to get the path. Example: /root/go/bin/babylond or /home/user/go/bin/babylond
+COS_PORT_RPC=          # default: 26657
+COS_VALOPER=           # example: bbnvaloper1234545636767376535673
+LOGENTRY=babylon
