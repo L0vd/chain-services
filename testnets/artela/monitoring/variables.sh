c4e8f017f30b682aca19246d75ee8491faa52d10
Jango | L0vd <49846495+NktSH@users.noreply.github.com>
2024-01-17 22:31:14 +0300

    Delete testnets/artela directory

--- testnets/artela/monitoring/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Artela monitoring variables template 
-COS_BIN_NAME=          # use "which artelad" command to get the path. Example: /root/go/bin/artelad or /home/user/go/bin/artelad
-COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: cosmosvaloper1234545636767376535673
-LOGENTRY=artela
