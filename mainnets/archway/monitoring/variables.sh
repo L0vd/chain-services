40dec637b10fc0cc3ebf8acee0a16ea4da2f1d99
Egozit <iampetroved@gmail.com>
2024-06-21 12:21:12 +0200

    Update gitbook

--- mainnets/archway/monitoring/variables.sh
+++ mainnets/archway/monitoring/variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Archway monitoring variables template 
-COS_BIN_NAME=          # use "which archwayd" command to get the path. Example: /root/go/bin/archwayd or /home/user/go/bin/archwayd
+COS_BIN_NAME=          # use "which archway" command to get the path. Example: /root/go/bin/archway or /home/user/go/bin/archway
 COS_PORT_RPC=          # default: 26657
-COS_VALOPER=           # example: nullvaloper1234545636767376535673
+COS_VALOPER=           # example: archwayvaloper1234545636767376535673
 LOGENTRY=archway
